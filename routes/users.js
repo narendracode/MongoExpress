f5776b2ea74855607f3210c4932180f5110e0e2b
Narendra Kumar Soni <narendrasoni2@gmail.com>
2014-09-23 17:38:45 +0800

    tested DB connection

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+/* get the controllers */
+var users = require('../app/controllers/userController');
+
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
+
+router.get('/create',users.create);
 
 module.exports = router;
